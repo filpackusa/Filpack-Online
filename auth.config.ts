bdae1864e01e88a995f7981c4ea1f3204b828a94
Melih Akdemir <melihakdemir@Melihs-MacBook-Pro.local>
2025-11-27 00:18:32 +0300

    Fix signin path to use NextAuth default API route

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,9 @@
 import type { NextAuthConfig } from "next-auth"
 
 export const authConfig = {
+    pages: {
+        signIn: "/api/auth/signin",
+    },
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user
